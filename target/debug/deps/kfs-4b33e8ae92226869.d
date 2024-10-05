e5a6bee5a9cdd912a31207b7fd25cc3f02e6a52c
FelixBrgm <github.badly321@passinbox.com>
2024-10-05 19:31:49 +0200

    init2

--- target/debug/deps/kfs-4b33e8ae92226869.d
+++
@@ -1,5 +0,0 @@
-/Users/code/source/kfs/target/debug/deps/libkfs-4b33e8ae92226869.rmeta: src/main.rs
-
-/Users/code/source/kfs/target/debug/deps/kfs-4b33e8ae92226869.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/kfs-bc3175dc4196ee02.d
+++
@@ -1,5 +0,0 @@
-/Users/code/source/kfs/target/debug/deps/kfs-bc3175dc4196ee02: src/main.rs
-
-/Users/code/source/kfs/target/debug/deps/kfs-bc3175dc4196ee02.d: src/main.rs
-
-src/main.rs:
