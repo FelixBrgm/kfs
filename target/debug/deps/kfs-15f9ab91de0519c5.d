c9be2cb6c34f694715dbfc34e6f438321ef0e2e5
code <code@Gooss-MacBook-Pro.local>
2024-10-05 19:31:49 +0200

    init2

--- target/debug/deps/kfs-15f9ab91de0519c5.d
+++
@@ -1,5 +0,0 @@
-/Users/code/source/kfs/target/debug/deps/kfs-15f9ab91de0519c5: src/main.rs
-
-/Users/code/source/kfs/target/debug/deps/kfs-15f9ab91de0519c5.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/kfs-aeb79394436da530.d
+++
@@ -1,5 +0,0 @@
-/Users/code/source/kfs/target/debug/deps/libkfs-aeb79394436da530.rmeta: src/main.rs
-
-/Users/code/source/kfs/target/debug/deps/kfs-aeb79394436da530.d: src/main.rs
-
-src/main.rs:
