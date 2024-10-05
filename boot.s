e56fdb4fe745297c3063ead7e613bb9a8daf3ea3
FelixBrgm <github.badly321@passinbox.com>
2024-10-05 19:55:25 +0200

    add: build instructions for bare metal

--- boot.s
+++
@@ -1,5 +0,0 @@
-mov rsp, 0x00000000 ; Should be the value of the stack 
-                    ; Load in the kernel
-
-
-jmp 0x00000000 ; Set Instruction Pointer to the location of the start of the kernel binary in memory
