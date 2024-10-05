ab2ad1a63cbb964d63a346a5119646873a091a3f
winstonallo <arthurbiedchar@gmail.com>
2024-10-05 19:55:25 +0200

    add: build instructions for bare metal

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,11 @@
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_infoo: &PanicInfo) -> ! {
-    // Handle the panic: For example, you could print or log the panic info.
-    // Since this is a kernel or bare-metal environment, you might want to halt the system.
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // Call our kernel initialization function (could include logging, etc.)
-    kernel_main();
-
     loop {}
 }
-
-pub fn kernel_main() {}
