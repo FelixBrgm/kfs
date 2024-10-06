8da1ab4b5505148b077aed85c10142e143629ebf
FelixBrgm <github.badly321@passinbox.com>
2024-10-06 19:38:13 +0200

    add: multiboot_header.s, linker.ld

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 all:
-	cargo build --release
-	cp target/x86_64-unknown-none/release/kfs iso/boot/kernel
+	cargo rustc --release -- --emit=obj
+	nasm -f elf64 multiboot_header.s
+	ld --nmagic --output=kernel --script=linker.ld multiboot_header.o target/x86_64-unknown-none/release/deps/kfs-e34396d0b60553df.o -z noexecstack
+	cp kernel iso/boot/kernel/kernel
 	grub-mkrescue -o kfs.iso ./iso
 	qemu-system-x86_64 -cdrom kfs.iso -boot d -nographic
 
---
+++ multiboot_header.s
@@ -0,0 +1,19 @@
+section .multiboot_header
+
+header_start: ; label -> allows to refer to specific part of code
+    ; dd -> define double word (word: sequence of 16 bits, like in networking)
+    dd 0xe85250d6                   ; magic number, completely arbitrary (try  if it does not work)
+    dd 0                            ; protected mode
+    dd header_end - header_start    ; header length
+
+    ; checksum -> 0x100000000 means unsigned 
+    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+
+    ; required end tag
+    dw 0    ; type
+    dw 0    ; flags
+    dw 0    ; size
+header_end:
+
+_start:
+    jmp _start
