c36ac1be66226ae747b365a96892e08d6a2a5901
winstonallo <arthurbiedchar@gmail.com>
2024-11-01 09:40:31 +0100

    fix: add separate default build script
    build.rs runs **before** rustc by default, so it would not find the object files emitted

--- Makefile
+++ Makefile
@@ -1,11 +1,12 @@
 all:
 	cargo rustc --release -- --emit=obj
-	nasm -f elf64 multiboot_header.s
-	ld --nmagic --output=kernel --script=linker.ld multiboot_header.o target/x86_64-unknown-none/release/deps/*.o -z noexecstack
-	rm multiboot_header.o
-	cp kernel iso/boot/kernel
-	rm kernel
-	grub-mkrescue -o kfs.iso ./iso
-	qemu-system-x86_64 -cdrom kfs.iso -boot d -nographic
+	cargo run
+	# nasm -f elf64 multiboot_header.s
+	# ld --nmagic --output=kernel --script=linker.ld multiboot_header.o target/x86_64-unknown-none/release/deps/*.o -z noexecstack
+	# rm multiboot_header.o
+	# cp kernel iso/boot/kernel
+	# rm kernel
+	# grub-mkrescue -o kfs.iso ./iso
+	# qemu-system-x86_64 -cdrom kfs.iso -boot d -nographic
 
 .PHONY: all
---
+++ build-script.rs
@@ -0,0 +1,77 @@
+use core::convert::From;
+use std::env;
+use std::path::{Path, PathBuf};
+use std::process::Command;
+
+fn main() {
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let multiboot_object_file = format!("{}/multiboot_header.o", out_dir);
+
+    assemble_multiboot_header(&multiboot_object_file);
+    link(&multiboot_object_file);
+}
+
+fn compile_kernel() {
+    let status = Command::new("cargo")
+        .args(&["rustc", "--release", "--", "--emit=obj"])
+        .status()
+        .expect("could not emit object files for kfs");
+    if !status.success() {
+        panic!("could not create kernel object file")
+    }
+}
+
+fn assemble_multiboot_header(multiboot_object_file: &str) {
+    let status = Command::new("nasm")
+        .args(&[
+            "-f",
+            "elf64",
+            "multiboot_header.s",
+            "-o",
+            &multiboot_object_file,
+        ])
+        .status()
+        .expect("failed to assemble multiboot_header.s");
+    if !status.success() {
+        panic!("nasm could not assemble multiboot_header.s")
+    }
+    println!("cargo:rerun-if-changed=multiboot_header.s");
+}
+
+fn link(multiboot_object_file: &str) {
+    let target_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap())
+        .join("target")
+        .join("x86_64-unknown-none")
+        .join("release");
+    let deps_dir = target_dir.join("deps");
+
+    let obj_file = std::fs::read_dir(&deps_dir)
+        .expect(&format!("failed to read directory '{:?}'", &deps_dir).to_string())
+        .filter_map(|entry| {
+            let entry = entry.ok()?;
+            let path = entry.path();
+            if path.extension()? == "o" {
+                Some(path)
+            } else {
+                None
+            }
+        })
+        .next()
+        .expect("no object file found in deps directory");
+
+    let status = Command::new("ld")
+        .args(&[
+            "--nmagic",
+            "--output=kernel",
+            "--script=linker.ld",
+            &multiboot_object_file,
+            obj_file.to_str().unwrap(),
+            "-z",
+            "noexecstack",
+        ])
+        .status()
+        .expect("failed to link kernel object file with assembled multiboot header");
+    if !status.success() {
+        panic!("ld failed during the linking process");
+    }
+}
