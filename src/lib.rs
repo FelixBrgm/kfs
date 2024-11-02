3098d23da935525cf24bad8bf55c7b1435e543ef
winstonallo <arthurbiedchar@gmail.com>
2024-11-02 14:06:53 +0100

    add: kfs1 done lol

--- src/lib.rs
+++ src/lib.rs
@@ -15,18 +15,7 @@ fn panic(_info: &PanicInfo) -> ! {
 pub extern "C" fn kernel_main() {
     let mut vga_buffer = vga::Buffer::new();
 
-    vga_buffer.putchar(b'H');
-    vga_buffer.putchar(b'e');
-    vga_buffer.putchar(b'l');
-    vga_buffer.putchar(b'l');
-    vga_buffer.putchar(b'o');
-    vga_buffer.putchar(b',');
-    vga_buffer.putchar(b' ');
-    vga_buffer.putchar(b'W');
-    vga_buffer.putchar(b'o');
-    vga_buffer.putchar(b'r');
-    vga_buffer.putchar(b'l');
-    vga_buffer.putchar(b'd');
-    vga_buffer.putchar(b'!');
+    vga_buffer.putchar(b'4');
+    vga_buffer.putchar(b'2');
     loop {}
 }
