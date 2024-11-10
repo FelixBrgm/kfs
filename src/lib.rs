f472f7def32269f5fd0814dd33443ea645f760e9
felixbrgm <200~github.badly321@passinbox.com>
2024-11-10 10:58:05 +0000

    vga to terminal

--- src/lib.rs
+++ src/lib.rs
@@ -5,25 +5,23 @@ fn main() {}
 
 pub mod gdt;
 pub mod idt;
-pub mod vga;
+pub mod terminal;
 
 #[no_mangle]
 #[allow(clippy::empty_loop)]
 pub extern "C" fn kernel_main() {
     let a = kernel_init();
 
-    let mut vga_buffer = vga::Buffer::new();
-    vga_buffer.clear_screen();
-    vga_buffer.set_colors(vga::VGAColor::Black, vga::VGAColor::Green);
-    match a {
-        true => vga_buffer.set_background_color(vga::VGAColor::Green),
-        false => vga_buffer.set_background_color(vga::VGAColor::Red)
-    }
-    vga_buffer.write_char_at(1, 0, b'4');
+    let mut terminal = Terminal::new();
+
+    terminal.clear_screen();
+    terminal.write_u8_arr("Hello World".as_bytes());
     loop {}
 }
 
 use core::{panic::PanicInfo, ptr};
+
+use terminal::Terminal;
 
 #[cfg(not(test))]
 #[panic_handler]
