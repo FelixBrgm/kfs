f1cfe9932a87c33438ad65aa021d918e3afdfa83
FelixBrgm <github.badly321@passinbox.com>
2024-11-10 09:36:07 +0100

    added boot code

--- assets/boot.s
+++ assets/boot.s
@@ -26,8 +26,23 @@
 	_start:
 		mov $stack_top, %esp
 
+
+		gdtr DW 0 ; For limit storage
+			DD 0 ; For base storage
+
+		XOR   EAX, EAX
+		MOV   AX, DS
+		SHL   EAX, 4
+		ADD   EAX, ''GDT''
+		MOV   [gdtr + 2], eax
+		MOV   EAX, ''GDT_end''
+		SUB   EAX, ''GDT''
+		MOV   [gdtr], AX
+		LGDT  [gdtr]
+
         push %eax
         push %ebx
+		cli
 		call kernel_main
  
 		hang:
