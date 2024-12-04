74a5ffa2265c8f559dc698cecd18868426f783e6
Felix Brüggemann <57171559+FelixBrgm@users.noreply.github.com>
2024-12-04 08:08:36 +0000

    added subject requirement

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,9 @@ mod terminal;
 #[no_mangle]
 pub extern "C" fn kernel_main() {
     let mut t = terminal::Terminal::new();
+    t.write(b'4');
+    t.write(b'2');
+    t.write(b'\n');
     t.flush();
     loop {
         if let Some(key) = terminal::ps2::read_if_ready() {
